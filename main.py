073a334277dea3083d5e2f2e7276c2bedd4ae3a7
ankurjdnks <awarikoo360@gmail.com>
2025-08-31 20:25:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1546,7 +1546,7 @@ async def universal_drm_handler(bot: Client, m: Message):
                             v_name = re.sub(r":.*", "", raw_title).strip()
                     
                         if caption == "/cc1":
-                            cc = f'[🎥]Vid Id : {str(count).zfill(3)}\n**Video Title :** `{v_name} [{res}p] .mkv`\n<blockquote><b>Batch Name : {b_name}\nTopic Name : {t_name}</b></blockquote>\n\n**Extracted by➤**{CR}\n'
+                            cc = f'𝐓𝐢𝐭𝐥𝐞 :** `{v_name}  ` \n\n**𝐓𝐨𝐩𝐢𝐜 : {t_name} \n\n**𝐁𝐚𝐭𝐜𝐡 : {b_name} \n\n**👨🏻‍💻 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐁𝐲 : **{CR}\n'
                             cc1 = f'[📕]Pdf Id : {str(count).zfill(3)}\n**File Title :** `{v_name} .pdf`\n<blockquote><b>Batch Name : {b_name}\nTopic Name : {t_name}</b></blockquote>\n\n**Extracted by➤**{CR}\n'
                             cczip = f'[📁]Zip Id : {str(count).zfill(3)}\n**Zip Title :** `{v_name} .zip`\n<blockquote><b>Batch Name : {b_name}\nTopic Name : {t_name}</b></blockquote>\n\n**Extracted by➤**{CR}\n'
                             ccimg = f'[🖼️]Img Id : {str(count).zfill(3)}\n**Img Title :** `{v_name} .jpg`\n<blockquote><b>Batch Name : {b_name}\nTopic Name : {t_name}</b></blockquote>\n\n**Extracted by➤**{CR}\n'
